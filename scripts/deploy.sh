480b411e076b06c69269e9a53680f8a7542b715f
dldylan <zjpjzdl@qq.com>
2019-03-30 03:21:58 +0800

    Update .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,3 @@
-git clone https://github.com/dldylan/dldylan.github.io.git
-cd dldylan.github.io
-git config --global user.name "dldylan"
-git config --global user.email "zjpjzdl@qq.com"
 git add .
 git commit -m "Auto Updated by Travis CI"
 git push --force --quiet "https://dldylan:CODING_TOKEN@git.dev.tencent.com/dldylan/dldylan.coding.me.git" master:master
