d633423305252a76580cc9660d219e5afd5e65c0
dldylan <zjpjzdl@qq.com>
2019-03-30 02:33:29 +0800

    Edit travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
-git init
+git clone https://github.com/dldylan/dldylan.github.io.git
+cd dldylan.github.io
 git config --global user.name "dldylan"
 git config --global user.email "zjpjzdl@qq.com"
 git add .
-git commit -m "Update Blog By Travis CI With Build $TRAVIS_BUILD_NUMBER"
-git push --force --quiet "https://dldylan:CODING_TOKEN@git.dev.tencent.com/dldylan/home.git" master:master
+git commit -m "Auto Updated by Travis CI"
+git push --force --quiet "https://dldylan:CODING_TOKEN@git.dev.tencent.com/dldylan/dldylan.coding.me.git" master:master
